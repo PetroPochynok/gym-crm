b858c17a269a90ed0a655453a3ca1dd69496f83d
Petro Pochynok <p.pochynok123@gmail.com>
2026-04-16 12:34:35 +0300

    add User model

---
+++ src/main/java/com/epam/gym/crm/model/User.java
@@ -0,0 +1,19 @@
+package com.epam.gym.crm.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private boolean isActive;
+
+}
